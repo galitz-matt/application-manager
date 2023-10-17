f83c2e9e9028ddbd2c537034aa530707802a10ab
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-10-17 17:45:55 -0400

    feat(application): implement getters and setters

--- source/application.py
+++ source/application.py
@@ -1,9 +1,25 @@
 from datetime import time
 
+
 class Application:
 
 	def __init__(self, company, position, date, status):
 		self._company: str = company
 		self._position: str = position
 		self._date: time = date
-		self._status:
+		self._status: status = status
+
+	def get_company(self):
+		return self._company
+
+	def get_position(self):
+		return self._position
+
+	def get_data(self):
+		return self._date
+
+	def get_status(self):
+		return self._status
+
+	def set_status(self, new_status):
+		self._status = new_status
